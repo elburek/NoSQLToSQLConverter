196f3a810af55204c0d81bf5f6b01395d130ab68
elburek <elburek@gmail.com>
2019-09-23 15:26:39 +0200

    Code refactor

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,36 +1,20 @@
 import com.google.gson.Gson;
-import com.mongodb.MongoClient;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoCursor;
-import com.mongodb.client.MongoDatabase;
 import models.Item;
 import org.bson.Document;
 
-import javax.print.Doc;
-import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        MongoClient mongoClient = new MongoClient("localhost", 27017);
-        MongoDatabase mongoDatabase = mongoClient.getDatabase("MGR");
-        MongoCollection collection = mongoDatabase.getCollection("items");
-        System.out.println(mongoDatabase.getName() + "." + collection.getNamespace().getCollectionName());
-        List<Document> items = extractListFromCollection(collection);
-        System.out.println("done");
+
+        MongoController mongoController = new MongoController("localhost", 27017, "MGR");
+        List<Document> items = mongoController.extractDocumentsFromCollection("items");
         Gson gson = new Gson();
-        Item item = gson.fromJson(items.get(0).append("dupa","dupa2").toJson(), Item.class);
+        Item item = gson.fromJson(items.get(0).append("dupa", "dupa2").toJson(), Item.class);
         System.out.println("2");
 
     }
 
-    private static List<Document> extractListFromCollection(MongoCollection collection) {
-        List<Document> items = new ArrayList<>();
-        MongoCursor x = collection.find().cursor();
-        while (x.hasNext()) {
-            items.add((Document)x.next());
-        }
-        return items;
-    }
+
 }
---
+++ src/main/java/MongoController.java
@@ -0,0 +1,33 @@
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MongoController {
+
+    private final MongoClient mongoClient;
+    private final MongoDatabase mongoDatabase;
+
+    public MongoController(final String host,
+                           final int port,
+                           final String databaseName) {
+        this.mongoClient = new MongoClient(host, port);
+        this.mongoDatabase = mongoClient.getDatabase(databaseName);
+    }
+
+
+    public List<Document> extractDocumentsFromCollection(String collectionName) {
+        MongoCollection collection = mongoDatabase.getCollection(collectionName);
+        System.out.println(mongoDatabase.getName() + "." + collection.getNamespace().getCollectionName());
+        List<Document> items = new ArrayList<>();
+        MongoCursor x = collection.find().cursor();
+        while (x.hasNext()) {
+            items.add((Document) x.next());
+        }
+        return items;
+    }
+}
